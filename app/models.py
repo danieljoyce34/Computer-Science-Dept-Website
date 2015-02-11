639ba357232ce5b231509ae37bd339bf2d2f7a17
Copemate <Jie@Xiaojies-MacBook-Pro.local>
2015-02-11 16:40:27 -0500

    Fixed table names and updated schema

--- app/models.py
+++ app/models.py
@@ -4,9 +4,9 @@ from app import db
 class Image(db.Model):
     __tablename__ = 'images'
     id = db.Column(db.Integer, primary_key=True)
-    image_type = db.Column(db.String(45))
+    image_type = db.Column(db.String(45), nullable=False)
     alt_text = db.Column(db.Text)
-    image_extension = db.Column(db.String(45))
+    image_extension = db.Column(db.String(45), nullable=False)
 
     def __repr__(self):
         return ('<Image id %i, image_type %s, alt_text %s, image_extension %s>'
@@ -14,12 +14,12 @@ class Image(db.Model):
 
 
 class Sideview(db.Model):
-    __tablename = 'sideviews'
+    __tablename__ = 'sideviews'
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text)
-    title = db.Column(db.String(45))
-    category = db.Column(db.String(45))
-    active = db.Column(db.Integer)
+    content = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(45), nullable=False)
+    category = db.Column(db.String(45), nullable=False)
+    active = db.Column(db.Integer, nullable=False)
     image_id = db.Column(db.Integer, db.ForeignKey('images.id'))
     image = db.relationship('Image', backref=db.backref('image', lazy='dynamic'),
                             uselist=False)
