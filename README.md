61203caa0e5bf332c139361937e460cfc4a39fe7
Copemate <Jie@Xiaojies-MacBook-Pro.local>
2015-02-11 15:43:21 -0500

    Updated readme file and db schema

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ All the packages that are currently being used are stored in the requirements.tx
 To use the file, first do a source activate to activate your virtualenv, then do 'pip install -r requirements.txt'
 
 ####Remember to name your virtualenv flask so it will be ignored when you commit
+
+##Accessing the database schema
+
+>Visit this link:
+http://ondras.zarovi.cz/sql/demo/
+Click 'Save / Load', then paste the content of the schema file to the the field and hit load
--- app/models.py
+++ app/models.py
@@ -1,27 +1,29 @@
 from app import db
 
-class Image(db.Model):
-	__tablename__ = 'images'
-	id = db.Column(db.Integer, primary_key=True)
-	image_type = db.Column(db.String(45))
-	alt_text = db.Column(db.Text)
-	image_extension = db.Column(db.String(45))
 
-	def __repr__(self):
-		return ('<Image id %i, image_type %s, alt_text %s, image_extension %s>'
-				% (self.id, self.image_type, self.alt_text, self.image_extension))
+class Image(db.Model):
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True)
+    image_type = db.Column(db.String(45))
+    alt_text = db.Column(db.Text)
+    image_extension = db.Column(db.String(45))
+
+    def __repr__(self):
+        return ('<Image id %i, image_type %s, alt_text %s, image_extension %s>'
+                % (self.id, self.image_type, self.alt_text, self.image_extension))
+
 
 class Sideview(db.Model):
-	__tablename = 'sideviews'
-	id = db.Column(db.Integer, primary_key=True)
-	content = db.Column(db.Text)
-	title = db.Column(db.String(45))
-	category = db.Column(db.String(45)) 
-	active = db.Column(db.Integer)
-	image_id = db.Column(db.Integer, db.ForeignKey('images.id'))
-	image = db.relationship('Image', backref=db.backref('image', lazy='dynamic'),
-							uselist=False)
+    __tablename = 'sideviews'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    title = db.Column(db.String(45))
+    category = db.Column(db.String(45))
+    active = db.Column(db.Integer)
+    image_id = db.Column(db.Integer, db.ForeignKey('images.id'))
+    image = db.relationship('Image', backref=db.backref('image', lazy='dynamic'),
+                            uselist=False)
 
-	def __repr__(self):
-		return ('<Sideview id %i, content %s, title %s, active %i, image_id %i>'
-				% (self.id, self.content, self.title, self.active, self.image_id))
+    def __repr__(self):
+        return ('<Sideview id %i, content %s, title %s, active %i, image_id %i>'
+                % (self.id, self.content, self.title, self.active, self.image_id))
