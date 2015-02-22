d7aa3c4213b6b62fa744d00370b4aa050a01d463
Copemate <Jie@Xiaojies-MacBook-Pro.local>
2015-02-22 18:08:09 -0500

    modified routes.py to output all json object

--- app/routes.py
+++ app/routes.py
@@ -7,13 +7,29 @@ from .models import Image, Sideview, News, Alert
 def index():
 	return render_template('index.html')
 
-@app.route('/alerts', methods=['GET'])
+@app.route('/testing', methods=['GET'])
 def alerts():
 	if request.method == 'GET':
 		alerts = Alert.query.all()
+		news = News.query.all()
+		images = Image.query.all()
+		sideviews = Sideview.query.all()
 
-		json_result = []
+		alert_result = []
+		news_result = []
+		image_result = []
+		sideview_result = []
 		for alert in alerts:
 			json = alert.to_json_format()
-			json_result.append(json)
-	return jsonify(alerts=json_result)
+			alert_result.append(json)
+		for new in news:
+			json = new.to_json_format()
+			news_result.append(json)
+		for image in images:
+			json = image.to_json_format()
+			image_result.append(json)
+		for sideview in sideviews:
+			json = sideview.to_json_format()
+			sideview_result.append(json)
+	return jsonify(alerts=alert_result, news=news_result, images=image_result,
+					sideviews=sideview_result)
