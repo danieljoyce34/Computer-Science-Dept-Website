6dbab8d18e600904bb50636ffdcb88b0300f8648
Copemate <Jie@Xiaojies-MacBook-Pro.local>
2015-02-25 15:26:58 -0500

    Modified routes.py to return individual json obj

--- app/routes.py
+++ app/routes.py
@@ -7,29 +7,38 @@ from .models import Image, Sideview, News, Alert
 def index():
 	return render_template('index.html')
 
-@app.route('/testing', methods=['GET'])
-def alerts():
+@app.route('/retrieveAlerts', methods = ['GET'])
+def alertsAjax():
 	if request.method == 'GET':
 		alerts = Alert.query.all()
-		news = News.query.all()
-		images = Image.query.all()
-		sideviews = Sideview.query.all()
 
 		alert_result = []
-		news_result = []
 		image_result = []
 		sideview_result = []
 		for alert in alerts:
 			json = alert.to_json_format()
 			alert_result.append(json)
+		return jsonify(alerts=alert_result, images=image_result,
+					sideviews=sideview_result)
+
+@app.route('/retrieveNews', methods = ['GET'])
+def newsAjax():
+	if request.method == 'GET':
+		news = News.query.all()
+
+		news_result = []
 		for new in news:
 			json = new.to_json_format()
 			news_result.append(json)
-		for image in images:
-			json = image.to_json_format()
-			image_result.append(json)
+		return jsonify(news=news_result)
+
+@app.route('/retrieveSideviews', methods = ['GET'])
+def sideviewsAjax():
+	if request.method == 'GET':
+		sideviews = Sideview.query.all()
+
+		sideview_result = []
 		for sideview in sideviews:
 			json = sideview.to_json_format()
 			sideview_result.append(json)
-	return jsonify(alerts=alert_result, news=news_result, images=image_result,
-					sideviews=sideview_result)
+		return jsonify(sideviews=sideview_result)
