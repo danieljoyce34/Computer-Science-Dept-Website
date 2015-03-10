198faa79917377eef1a0f91093ba8f87fb9027be
Prokow01 <peterrokowski@gmail.com>
2015-03-10 18:05:29 -0400

    added article route and stuff

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,19 @@ def index():
 @app.route('/carousel')
 def carousel():
 	return render_template('carousel.html')
+
+@app.route('/article', methods=['POST', 'GET'])
+	e=request.args['articleNumber']
+
+	#QUERY DATABASE HERE
+	results = {}
+	results['articleImage'] ='120003' 
+	results['articleHeader']='There is some data'
+	results['articleContent']='We have contempt for our content'
+
+	return render_template('article', data=results)
+
+@app.route('/getArticleNumber')
+def getArticleNumber():
+	return 12
+
