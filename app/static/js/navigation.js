40487e28ab76bdc256d8bb5360402d5b5f3f2022
William Makabenta <wmakaben@gmail.com>
2015-03-11 16:02:40 -0400

    fixed menu shift

--- app/static/js/navigation.js
+++ app/static/js/navigation.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
 	$('#nav-switch').click(function(){
 		if($('#nav').css('display') == 'none'){
 			$('#nav').show();
-			$('#content-wrapper').css('left', 160);
+			$('#content-wrapper').css('left', 200);
+			$('#content-wrapper').css('right', -200);
 		}
 		else{
 			$('#nav').hide();
 			$('#content-wrapper').css('left', 0);
+			$('#content-wrapper').css('right', 0);
 		}
 	})
 });
