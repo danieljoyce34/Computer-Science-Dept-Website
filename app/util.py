7356e37102dd3b9debec78a57e8e81abd4d48ef7
Sahithi <syalama5@villanova.edu>
2015-03-18 17:04:09 -0400

    Added utility function to retrieve time from dateTime object

--- app/util.py
+++ app/util.py
@@ -7,3 +7,18 @@ def _next_month():
 	month = today.month % 12 + 1
 	day = min(today.day, calendar.monthrange(year, month)[1])
 	return datetime.date(year, month, day)
+
+def _time(dateTime):
+    hour=dateTime.hour
+    minute=dateTime.minute
+    if minute<10 :
+        strminute = '0' + str(minute)
+    else :
+        strminute = str(minute)
+    if hour>12 :
+        mhour = (hour-12)
+        return str(mhour) + ':'+ strminute + ' PM'
+    elif hour==12 :
+        return str(hour) + ':'+ strminute + ' PM'
+    else :
+        return str(hour) + ':'+ strminute + 'AM'
