3b25a97969487fc9cb90735f15e443a924f06287
William Makabenta <wmakaben@gmail.com>
2015-03-25 16:42:54 -0400

    fixed responsive nav

--- app/static/js/navigation.js
+++ app/static/js/navigation.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
+
+	// Resonsive navigation button - shows/hides side navbar
 	$('#nav-switch').click(function(){
 		if($('#nav').css('display') == 'none'){
 			$('#nav').show();
+			$('#content-wrapper').children().css('pointer-events', 'none');
 			// Slide animation for the responsive navigation bar
 			$('#content-wrapper').animate({
 				left: 200,
 				right: -200
 			}, 200, function(){
 			});
-			//$('#content-wrapper').css('left', 200);
-			//$('#content-wrapper').css('right', -200);
 		}
 		else{
 			// Slide animation for the responsive navigation bar
@@ -18,17 +19,18 @@ $(document).ready(function(){
 				right: 0
 			}, 200, function(){
 				$('#nav').hide();
+				$('#content-wrapper').children().css('pointer-events', 'auto');
 			});
-			//$('#content-wrapper').css('left', 0);
-			//$('#content-wrapper').css('right', 0);
 		}
 	});
 
-	$('.nav-item').on('swipedown', function(){
+	// For Mobile devices - shows the submenu options when a menu item is swiped down
+	$('.nav-item').on('swiperight', function(){
 		$('#nav').find('.subnav').hide();
-		$(this).find('.subnav').show();
+		$(this).find('.subnav').slideDown(125);
 	});
 
+	// Hides the responsive sidenav bar when the page content is clicked
 	$('#content-wrapper').click(function(){
 		if($(this).css('left') == '200px'){
 			$(this).animate({
@@ -36,7 +38,14 @@ $(document).ready(function(){
 				right: 0
 			}, 200, function(){
 				$('#nav').hide();
+				$('#content-wrapper').children().css('pointer-events', 'auto');
 			});
 		}
 	});
+
+	// Slide transition for displaying submenu options on hover
+	$('.nav-item').hover(
+		function(){ $(this).children('.subnav').slideDown(125); },
+		function(){ $(this).children('.subnav').slideUp(125); }
+	);
 });
