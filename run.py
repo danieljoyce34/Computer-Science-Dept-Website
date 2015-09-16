a0a56474929904db02d5c659b4c01f08d10dcbf6
Copemate <xjiang.270@gmail.com>
2015-09-16 16:53:26 -0400

    added ssl support, please use https

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 #!../csc_env/bin/python
 from app import app
+import ssl
 
-app.run(debug=True)
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+context.load_cert_chain('server.crt', 'server.key')
+
+app.run(debug=True, ssl_context=context)
 # host='0.0.0.0'
