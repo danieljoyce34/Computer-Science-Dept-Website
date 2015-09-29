00f80023e61e573bfdbd946c6914720b118f2733
Copemate <xjiang.270@gmail.com>
2015-09-29 19:45:53 -0400

    initial setup for image upload

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://xjadmin:SprinG2015@webster.csc.villanova.edu/xjdb'
 #SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:@localhost/xjdb'
+UPLOAD_FOLDER = 'static/images/'
+ALLOWED_EXTENSION = set(['png', 'jpg', 'jpeg', 'gif'])
