e81e43e3210061815b7dc0ed995eb44e6fac0c68
Copemate <xjiang.270@gmail.com>
2015-10-18 21:54:03 -0400

    change html to show logout option if user is logged in already

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.secret_key = 'shhh this is a secret'
 db = SQLAlchemy(app)
 loginManager = LoginManager()
 loginManager.init_app(app)
+loginManager.login_view = 'login'
 
 from app import routes, models
