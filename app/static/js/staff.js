ec97b7bc411a33d79a96f647c1bfb2da7da7e163
William Makabenta <wmakaben@gmail.com>
2015-10-26 15:04:22 -0400

    cleaned up code in faculty.html and staff.js

--- app/static/js/staff.js
+++ app/static/js/staff.js
@@ -1,70 +1,16 @@
 
-var types = ["fulltime", "adjunct", "other"];
-
-var staff = [
-	{
-		"name" : "staff1",
-		"position" : ["instructor"],
-		"email" : "staff1@gmail.com",
-		"type" : "fulltime"
-	},
-	{
-		"name" : "staff2",
-		"position" : ["professor"],
-		"email" : "staff2@gmail.com",
-		"type" : "fulltime"
-	},
-	{
-		"name" : "staff3",
-		"position" : ["assistant"],
-		"email" : "staff3@gmail.com",
-		"type" : "adjunct"
-	},
-	{
-		"name" : "staff4",
-		"position" : ["administrator"],
-		"email" : "staff4@gmail.com",
-		"type" : "other"
-	},
-	{
-		"name" : "staff5",
-		"position" : ["professor", "chair"],
-		"email" : "staff5@gmail.com",
-		"type" : "fulltime"
-	},
-	{
-		"name" : "staff6",
-		"position" : ["adjunct instructor"],
-		"email" : "staff6@gmail.com",
-		"type" : "adjunct"
-	}
-];
-
 $(document).ready(function () {
 
-	/*
-	$.get('/retrievePeople', function (api_staff) {
-			console.log(api_staff);
-			staff = api_staff.people;
-			getTypes();
-			getStaff('all');
-	});
-	*/
-
 	$('#type-filter').change(function(){
-		//$('#staff-list').find('div.staff-container').remove();
-
 		var type = $(this).val().toLowerCase();
 
-		if (type == 'all') {
+		if (type == 'all') 
 			$('#staff-list>div.staff-container').show();
-		}
 		else {
 			$('#staff-list>div.staff-container').each(function(){
 				$(this).find('.staff-email').text().toLowerCase() == type ? $(this).show() : $(this).hide();
 			});
 		}
-		//getStaff($(this).val());
 	});
 
 	$('#search-filter').keyup(function(){
@@ -76,34 +22,8 @@ $(document).ready(function () {
 
 	$('#filter-reset').click(function(){
 		$('#type-filter').prop('selectedIndex',0);
-		getStaff('all');
 		$('#search-filter').val('');
 		$('#staff-list').find('.staff-container').show();
 	});
 
 });
-
-function getTypes() {
-	for(var i=0; i<types.length; i++)
-		$('#type-filter').append($('<option>').attr("value", types[i]).text(types[i]));
-}
-
-function getStaff (type) {
-	$('#staff-list').find('.staff-container').remove();
-	for(var i = 0; i < staff.length; i++){
-		if(type == 'all' || type == staff[i].type){
-			//console.log(staff[i].image_url);
-			//$('#staff-list').append($('<div class="staff-container">')
-			//		.append($('<div class="staff-name">').text(staff[i].name))
-			//		.append($('<div class="staff-position">').text(staff[i].job_title))
-			//		.append($('<div class="staff-email">').text(staff[i].person_type))
-			//		.prepend($('<div class="profile_image">').css('background-image', 'url(' + staff[i].image_url + ')').css('height', 111)));
-			$('#staff-list').append($('<div class="staff-container">')
-				.append($('<img src="' + staff[i].image_url + '" class="profile_image">'))
-				.append($('<div class="staff-name">').text(staff[i].name))
-				.append($('<div class="staff-position">').text(staff[i].job_title))
-				.append($('<div class="staff-email">').text(staff[i].person_type))
-			);
-		}
-	}
-}
