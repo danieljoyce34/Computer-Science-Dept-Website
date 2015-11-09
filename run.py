8f0b7b87dbc949e56bc0592e875ed1ce868b8213
Copemate <xjiang.270@gmail.com>
2015-11-09 15:25:44 -0500

    accept incoming connection

--- run.py
+++ run.py
@@ -5,5 +5,4 @@ import ssl
 context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
 context.load_cert_chain('server.crt', 'server.key')
 
-app.run(debug=True, ssl_context=context)
-# host='0.0.0.0'
+app.run(debug=True, host='0.0.0.0', ssl_context=context)
